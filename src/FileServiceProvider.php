be16705600c4ea0852d8b8cb3f454853d6b7d0a0
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-28 09:14:11 +0330

    bind contract to its implementation

--- src/FileServiceProvider.php
+++ src/FileServiceProvider.php
@@ -2,16 +2,20 @@
 
 namespace Celysium\File;
 
+use Celysium\File\Repositories\FileServiceInterface;
+use Celysium\File\Repositories\FileServiceRepository;
 use Illuminate\Support\ServiceProvider;
 
 class FileServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        $this->app->bind(FileServiceInterface::class, FileServiceRepository::class);
+    }
+
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-    }
-
-    public function register()
-    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
     }
 }
