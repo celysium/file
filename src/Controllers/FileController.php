7cf0cfe11828989a6c3fd19ab4e2636dd08eac8f
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-28 09:14:29 +0330

    Add controllers

---
+++ src/Controllers/FileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Celysium\File\Controllers;
+
+class FileController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function list()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
