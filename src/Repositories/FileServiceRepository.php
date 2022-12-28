91eb631a8802a9cb2174d9d8853fadb59436219a
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-28 23:10:14 +0330

    Write body for create method in repository

--- src/Repositories/FileServiceRepository.php
+++ src/Repositories/FileServiceRepository.php
@@ -2,6 +2,11 @@
 
 namespace Celysium\File\Repositories;
 
+use Celysium\File\Models\File;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+
 class FileServiceRepository implements FileServiceInterface
 {
 
@@ -10,9 +15,20 @@ class FileServiceRepository implements FileServiceInterface
         // TODO: Implement list() method.
     }
 
-    public function create()
+    public function create(array $parameters): Model|Builder
     {
-        // TODO: Implement create() method.
+        /** @var UploadedFile $image */
+        $image = $parameters['image'];
+
+        $extension = $image->extension();
+
+        $path = $image->store('images', 'public');
+
+        return File::query()
+            ->create(array_merge($parameters, [
+                'path' => $path,
+                'extension' => $extension
+            ]));
     }
 
     public function delete()
