cc5f6d833b753e127d24c120a573f67e52fb6fe5
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-29 08:45:15 +0330

    Use store method instead of create

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('api')->prefix('api/file')->name('file.')->group(function () {
 
     Route::get('list', [FileController::class, 'list']);
-    Route::post('list', [FileController::class, 'create']);
-    Route::delete('list', [FileController::class, 'delete']);
+    Route::post('', [FileController::class, 'store']);
+    Route::delete('', [FileController::class, 'delete']);
 });
