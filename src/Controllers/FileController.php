c9fdf8a588c0b60dd702e040cf8e4e914a2752fc
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-29 08:45:38 +0330

    Complete controllers

--- src/Controllers/FileController.php
+++ src/Controllers/FileController.php
@@ -4,6 +4,11 @@ namespace Celysium\File\Controllers;
 
 use Celysium\File\Repositories\FileServiceInterface;
 use Celysium\File\Requests\CreateFileRequest;
+use Celysium\File\Requests\DeleteFileRequest;
+use Celysium\File\Resources\FileResource;
+use Celysium\Responser\Responser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
@@ -11,20 +16,24 @@ class FileController extends Controller
     {
     }
 
-    public function list()
+    public function list(Request $request): JsonResponse
     {
+        $listedFiles = $this->fileService->list($request->all());
 
+        return Responser::collection(FileResource::collection($listedFiles));
     }
 
-    public function create(CreateFileRequest $request)
+    public function store(CreateFileRequest $request): JsonResponse
     {
-        $validatedData = $request->validated();
+        $createdFile = $this->fileService->store($request->validated());
 
-        return $this->fileService->create($validatedData);
+        return Responser::created(new FileResource($createdFile));
     }
 
-    public function delete()
+    public function delete(DeleteFileRequest $request): JsonResponse
     {
-
+        return Responser::deleted(
+            $this->fileService->delete($request->validated())
+        );
     }
 }
