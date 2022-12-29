74bcd42d6803b18114106818c530ae9d6d1f35f0
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-29 08:48:47 +0330

    Write body of the methods

--- src/Repositories/FileServiceRepository.php
+++ src/Repositories/FileServiceRepository.php
@@ -3,36 +3,51 @@
 namespace Celysium\File\Repositories;
 
 use Celysium\File\Models\File;
+use Celysium\File\Models\Fileable;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileServiceRepository implements FileServiceInterface
 {
 
-    public function list()
+    public function list(array $parameters): LengthAwarePaginator
     {
-        // TODO: Implement list() method.
+        return File::query()
+            ->paginate($parameters['paginate_per_page'] ?? 20);
     }
 
-    public function create(array $parameters): Model|Builder
+    public function store(array $parameters): Model|Builder
     {
         /** @var UploadedFile $image */
         $image = $parameters['image'];
 
-        $extension = $image->extension();
-
-        $path = $image->store('images', 'public');
+        $path = Storage::putFile('images', $image, 'public');
 
         return File::query()
             ->create(array_merge($parameters, [
-                'path' => $path,
-                'extension' => $extension
+                'path' => $path
             ]));
     }
 
-    public function delete()
+    public function delete(array $parameters): bool
     {
-        // TODO: Implement delete() method.
+        $fileIds = File::query()
+            ->select('id')
+            ->where('path', $parameters)
+            ->get()
+            ->toArray();
+
+        Fileable::query()
+            ->whereIn('file_id', $fileIds)
+            ->delete();
+
+        File::query()
+            ->whereIn('id', $fileIds)
+            ->delete();
+
+        return Storage::delete($parameters);
     }
 }
