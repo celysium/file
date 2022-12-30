bc12560a4a9cc0adf58658cd93e462a9aab9de55
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-30 22:24:03 +0330

    Add name to routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->prefix('api/file')->name('file.')->group(function () {
 
-    Route::get('list', [FileController::class, 'list']);
-    Route::post('', [FileController::class, 'store']);
-    Route::delete('', [FileController::class, 'delete']);
+    Route::get('list', [FileController::class, 'list'])->name('list');
+    Route::post('', [FileController::class, 'store'])->name('store');
+    Route::delete('', [FileController::class, 'delete'])->name('delete');
 });
