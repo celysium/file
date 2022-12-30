63b6cc275eb941bb18e621a7930f2a3c3c0d6577
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-30 22:23:34 +0330

    Add file factory

---
+++ database/factories/FileFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\File\database\factories;
+
+use App\Models\File;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\File>
+ */
+class FileFactory extends Factory
+{
+
+    protected $model = File::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory()->create()->id,
+        ];
+    }
+}
