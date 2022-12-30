f61b9d2a7cab05f643e4fc2e6337bff0b24bb056
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-30 20:22:40 +0330

    Add extra type declaration

--- src/Repositories/FileRepositoryInterface.php
+++ src/Repositories/FileRepositoryInterface.php
@@ -4,11 +4,12 @@ namespace Celysium\File\Repositories;
 
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 interface FileRepositoryInterface
 {
-    public function list(array $parameters): LengthAwarePaginator;
+    public function list(array $parameters): LengthAwarePaginator|Collection;
 
     public function store(array $parameters): Model|Builder;
 
