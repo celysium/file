468540421a1accf3a759be731f99e0f58fda5468
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2023-01-02 11:48:36 +0330

    Rename list to index in routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('api')->prefix('api/file')->name('file.')->group(function () {
 
-    Route::get('list', [FileController::class, 'list'])->name('list'); // TODO index
+    Route::get('list', [FileController::class, 'index'])->name('index'); // TODO index
     Route::post('/', [FileController::class, 'store'])->name('store');
     Route::delete('/', [FileController::class, 'delete'])->name('delete');
 });
