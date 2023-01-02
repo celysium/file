4d61d48f34f0a6d56e49cea08e14d76efe983c27
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2023-01-02 11:49:31 +0330

    extends from base structure

--- src/Repositories/FileRepositoryInterface.php
+++ src/Repositories/FileRepositoryInterface.php
@@ -2,16 +2,9 @@
 
 namespace Celysium\File\Repositories;
 
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
+use Celysium\BaseStructure\Repository\BaseRepositoryInterface;
 
-interface FileRepositoryInterface
+interface FileRepositoryInterface extends BaseRepositoryInterface
 {
-    public function list(array $parameters): LengthAwarePaginator|Collection;
 
-    public function store(array $parameters): Model|Builder;
-
-    public function delete(array $parameters): bool;
 }
