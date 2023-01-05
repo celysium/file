6e6bfacb1a126fea19fbae987d471cc1e9a65915
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2023-01-05 21:20:39 +0330

    Add extension and new state function

--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Celysium\File\Models\File;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
 
 
 class FileFactory extends Factory
@@ -11,10 +12,23 @@ class FileFactory extends Factory
 
     protected $model = File::class;
 
-    public function definition()
+    public function definition(): array
     {
         return [
             'path' => $this->faker->filePath(),
+            'description' => $this->faker->sentence(),
+            'extension' => $this->faker->fileExtension(),
         ];
+    }
+
+    public function formData(): FileFactory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+              'file' => UploadedFile::fake()->image(
+                  $this->faker->file() . '.' . $this->faker->fileExtension()
+              )
+            ];
+        });
     }
 }
