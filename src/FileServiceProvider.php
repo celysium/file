fd152f2e83d80c504893f5fde5600c7a6bc5ed91
nasser <hekmati.nasser@gmail.com>
2023-01-10 11:37:10 +0330

    fix service provider

--- src/FileServiceProvider.php
+++ src/FileServiceProvider.php
@@ -8,15 +8,15 @@ use Illuminate\Support\ServiceProvider;
 
 class FileServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->app->bind(FileRepositoryInterface::class, FileRepository::class);
-    }
-
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
         $this->loadTranslationsFrom(__DIR__ . '/../lang', 'file');
+    }
+
+    public function register()
+    {
+        $this->app->bind(FileRepositoryInterface::class, FileRepository::class);
     }
 }
